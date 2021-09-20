3748ddf6b309e952e23b685c24bd6bfc907cf901
Max <max.edison@yahoo.com>
2021-09-20 15:29:46 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+//"grabs" the DODGER (I love that verb, haha)
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+          
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+      // moves the DODGER to the left
+    }}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+    //moves the DODGER to the right
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+      //moves DODGER to the left if the "event" is the Left Arrow Key
+    }
+    else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+        //moves DODGER to the right if the "event" is the Right Arrow key
+    }
+});
